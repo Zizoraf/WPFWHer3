fc7e2176ea88b30bbe18fa996204db2ad35f0d08
Zizoraf <mulders71@gmail.com>
2024-05-14 21:35:12 +0200

    working test DB start
    .

--- Project.Tests/DirectorControllerTests.cs
+++ Project.Tests/DirectorControllerTests.cs
@@ -4,6 +4,7 @@ using Project.Controllers;
 using Moq;
 using Project.Data;
 using Project.Models;
+using Project.Tests.HelpTestScripts;
 
 namespace Project.Tests;
 
@@ -22,80 +23,21 @@ public class DirectorControllerTests
     [Fact]
     public async Task GetDirectors()
     {
-        var director = new List<Director>() //VOEG CLASSES TOE VAN LINK IN TEAMS!!!
+        var director = new List<Director>()
         {
-            new Director() { }
+            new() { Id = 1, Name = "Henk", Films = new List<Film>()},
+            new() { Id = 1, Name = "Ruttie", Films = new List<Film>()},
+            new() { Id = 1, Name = "Bartolomeo", Films = new List<Film>()},
         }.AsQueryable();
 
         _mock.Setup(ctx => ctx.Directors).Returns(DbSetMock.Create(director));
 
         DirectorController controller = new DirectorController(_mock.Object, _mockGreeting.Object);
 
-        var result = controller.GetDirector(1);
+        var result = await controller.GetDirector(1);
 
-        var actioResult = Assert.IsType<ActionResult<IEnumerable<DirectorDTO>>>(result);
-        var directorDto = Assert.IsAssignableFrom<IEnumerable<DirectorDTO>>(actioResult.Value);
-        Assert.Equal(3, directorDto.Count());
+        var actionResult = Assert.IsType<ActionResult<DirectorDTO>>(result);
+        var directorDto = Assert.IsAssignableFrom<DirectorDTO>(actionResult.Value);
+        Assert.Equal(directorDto.Name, "Henk");
     }
-    
-    [Fact]
-    public void TestGreetingCall()
-    {
-        //Arrange
-        Mock<IGreetingDependency> mock = new();
-        mock.Setup(m => m.Greeting());
-        DirectorController controller = new DirectorController(null, mock.Object);
-        
-        //Act
-        controller.GetGreeting();
-        
-        //Assert
-        mock.Verify(m => m.Greeting(), Times.Once);
-    }
-    
-
-    [Fact]
-    public async Task GetAllReviewsTest()
-    {
-        var options = new DbContextOptionsBuilder<BioscoopContext>()
-            .UseInMemoryDatabase(databaseName: "ReviewListDatabase")
-            .Options;
-
-        // Insert seed data into the database using one instance of the context
-        using (var context = new BioscoopContext(options))
-        {
-            context.Reviews.Add(new Review { Id = 1, Score = 5, Description = "Great", CreationDate = DateTime.Now, Username = "user1" });
-            context.Reviews.Add(new Review { Id = 2, Score = 4, Description = "Enjoyed it", CreationDate = DateTime.Now, Username = "user2" });
-            context.Reviews.Add(new Review { Id = 3, Score = 3, Description = "Not bad", CreationDate = DateTime.Now, Username = "user3" });
-            context.SaveChanges();
-        }
-
-        // Use a clean instance of the context to run the test
-        using (var context = new BioscoopContext(options))
-        {
-            ReviewController reviewController = new ReviewController(context);
-            ActionResult<IEnumerable<ReviewDTO>> result = await reviewController.GetReviews();
-        
-            var reviews = result.Value;
-            Assert.NotNull(reviews);
-        
-            Assert.Equal(3, reviews.Count());;
-        }
-    }
-    
-    [Fact]
-    public void CreateBlog_saves_a_blog_via_context()
-    {
-        var mockSet = new Mock<DbSet<Review>>();
-
-        
-        mockContext.Setup(m => m.Reviews).Returns(mockSet.Object);
-
-        var service = new BlogService(mockContext.Object);
-        service.AddBlog("ADO.NET Blog", "http://blogs.msdn.com/adonet");
-
-        mockSet.Verify(m => m.Add(It.IsAny<Blog>()), Times.Once());
-        mockContext.Verify(m => m.SaveChanges(), Times.Once());
-    }
-    
 }
---
+++ Project.Tests/HelpTestScripts/DbSetMock.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Project.Tests.HelpTestScripts; 
+
+public static class DbSetMock
+{
+    public static DbSet<T> Create<T>(IQueryable<T> data) where T : class
+    {
+        var mockSet = new Mock<DbSet<T>>();
+        mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(new TestAsyncQueryProvider<T>(data.Provider));
+        mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+        mockSet.As<IAsyncEnumerable<T>>().Setup(m => m.GetAsyncEnumerator(It.IsAny<System.Threading.CancellationToken>())).Returns(new AsyncEnumerator<T>(data.GetEnumerator()));
+        mockSet.Setup(m => m.FindAsync(It.IsAny<object[]>()))
+            .ReturnsAsync((object[] ids) => data.FirstOrDefault(d => ids.Contains(d.GetType().GetProperty("Id").GetValue(d))));
+
+        return mockSet.Object;
+    }
+
+}
